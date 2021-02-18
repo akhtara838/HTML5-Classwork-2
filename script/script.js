2b6f10527d56ced8ebbe81a37f9f6bc1c5d39164
Ayesha Akhtar <ayeshaakhtar13@gmail.com>
2021-02-18 04:45:02 -0700

    errormsg

--- script/script.js
+++ script/script.js
@@ -12,3 +12,33 @@ function updateCountryView()
         divCountry.classList.add("invisible");
     }
 }
+
+function ValidateForm()
+{
+    var DoB = document.querySelector("#dob");
+    var errorr = document.querySelector("#doberror");
+    if(DoB.value == "")
+    {
+        errorr.classList.remove("invisible");
+        errorr.innerHTML = "The Date of Birth cannot be empty.";
+        DoB.classList.add("hasError");
+    }
+    else
+    {
+        var dobdate = new Date(DoB.value);
+        var today = new Date();
+        if(dobdate >= today)
+        {
+            errorr.classList.remove("invisible");
+            errorr.innerHTML = "The Date of Birth must be before today's date.";
+            DoB.classList.add("hasError");
+        }
+        else{
+            errorr.classList.add("invisible");
+            errorr.innerHTML = "";
+            DoB.classList.remove("hasError");
+        }
+
+    }
+    return false;
+}
